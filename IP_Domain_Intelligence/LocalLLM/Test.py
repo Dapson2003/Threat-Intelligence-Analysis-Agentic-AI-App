923daae0b0778545c23884b2361e3f6097c04819
Dapson2003 <120316953+Dapson2003@users.noreply.github.com>
2025-06-27 11:42:04 +0700

    Fixed : Log Location
    Now log is save at the correct location.

--- IP_Domain_Intelligence/LocalLLM/Test.py
+++ IP_Domain_Intelligence/LocalLLM/Test.py
@@ -2,5 +2,5 @@ import socket
 
 domain = "Google.com"
 ip_address = socket.gethostbyname(domain)
-open(".\IP_Domain_intelligence\log.txt", "a").write(f"Resolved {domain} -> {ip_address}\n")
+open("./IP_Domain_intelligence/log.txt", "a").write(f"Resolved {domain} -> {ip_address}\n")
 print(f"Resolved {domain} -> {ip_address}\n")
