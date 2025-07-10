86bdf5c65ccd359cc15abaaddfc00a39c33ede93
Dapson2003 <120316953+Dapson2003@users.noreply.github.com>
2025-07-10 10:56:56 +0700

    Update : Add toggle on auto server start up
    Now in env file you can set if the model is connected(sub) to the server including creating the Stream it will be using.
    #I should change the sub and pub name too fit into the env. (Maybe next commit)

--- RunModel/app.py
+++ RunModel/app.py
@@ -1,4 +1,5 @@
 # app.py
+import os
 from fastapi import FastAPI
 from apis.root import root_router
 from apis.PredictOne import predict_router
@@ -8,10 +9,17 @@ from apis.MatchMitre import match_mitre_router
 from Connection_to_Nats import OpenModelServer
 
 app = FastAPI(title="Agent Model API")
-#OpenModelServer
+#OpenModelServer base on environment variable
 @app.on_event("startup")
 async def startup_event():
-    await OpenModelServer()  # ✅ Automatically run this at startup
+    auto_open = os.getenv("AUTO_OPEN_CONNECTION", "false").lower()
+
+    if auto_open == "true":
+        print("🔌 AUTO_OPEN_CONNECTION is enabled. Connecting...")
+        await OpenModelServer()
+    else:
+        print("❌ AUTO_OPEN_CONNECTION is disabled. Skipping startup connection.")
+        
 # Route registration
 app.include_router(root_router)  # Handles "/"
 app.include_router(predict_router, prefix="/api")
