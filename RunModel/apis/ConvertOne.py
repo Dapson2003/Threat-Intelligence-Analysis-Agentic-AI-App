d6d1b42e46c9c88d5a01b31d7af34d8a9a9b1250
Dapson2003 <120316953+Dapson2003@users.noreply.github.com>
2025-07-14 10:15:02 +0700

    Refactor : Add Runmodel_Api_Example
    API Model example has been update to show the example input albeit pretty unreadable. But easy to click with swaggerUI. the default value is untouch for the ease of debugging

--- RunModel/apis/ConvertOne.py
+++ RunModel/apis/ConvertOne.py
@@ -1,13 +1,17 @@
 # apis/convertone.py
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 from typing import Dict, Any
 from model.convert_input_format import convert_input_format
+from apis.Example_Log_Keeper import example_log_body as example_log
 
 convert_router = APIRouter()
 
 class NodeWrapper(BaseModel):
-    node: Dict[str, Any]
+    node: Dict[str, Any] = Field(
+        #default=example_log["node"], # This is the default value
+        example=example_log["node"]  # This is the example shown in Swagger UI
+    )
 
 @convert_router.post("/convert")
 async def convert(alert: NodeWrapper):
--- RunModel/apis/MatchMitre.py
+++ RunModel/apis/MatchMitre.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 from typing import Dict
 from model.MatchMitre import optimized_match
-
+from apis.Example_Log_Keeper import example_prediction_body as example_log
 match_mitre_router = APIRouter()
 
 CSV_PATH = "./model/MitreMatch.csv"
 
 class PredictionRequest(BaseModel):
-    prediction: Dict[str, list]
+    prediction: Dict[str, list] = Field(
+    #default=example_log["prediction"],  # This is the default value
+    example = example_log["prediction"]  # This is the example shown in Swagger UI
+    )
 
 @match_mitre_router.post("/match-mitre")
 async def match_mitre(request: PredictionRequest):
